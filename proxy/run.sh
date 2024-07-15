6715baef2bc903da810e1017c84ab2a2aa07a0c9
jrutanen <rautasissi@gmail.com>
2024-07-15 22:41:47 +0200

    Updated for running static files from a volume

--- proxy/run.sh
+++ proxy/run.sh
@@ -4,4 +4,4 @@ set -e
 
 envsubst < /etc/nginx/default.conf.tpl > /etc/nginx/conf.d/default.conf
 nginx -g 'daemon off;'
-ln -s /vol/web/frontend /etc/nginx/conf.d/default.conf
+ln -s /frontend-files/build /usr/share/nginx/html
